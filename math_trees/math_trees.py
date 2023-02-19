f0c2acbfe3f8cb62553ef9c64c295d32b1c84924
Francisco Salinas <remixielive@live.com>
2023-02-19 14:36:37 -0500

    Add code for steps 1,2,3 and 5.

--- math_trees/math_trees.py
+++ math_trees/math_trees.py
@@ -1,1 +1,99 @@
 """Main module."""
+
+from collections import deque
+from textwrap import wrap
+
+order = {"*": 1, "/": 1, "+": 2, "-": 2, "(": 3}
+
+# In order to turn the given infix expression into an expression tree,
+# it's best to convert it to a postfix first
+
+
+def convertToPostfix(infix):
+    stack = deque()
+    postfix = ''
+
+    infix = infix.split(' ')
+
+    for char in infix:
+
+        # Step 5: Make the string parser from step 3 handle parentheses ()
+        if char == '(':  # add ( to the stack to recursively search for characters inside parentheses
+            stack.append(char)
+
+        # begin adding values to postfix and remove from the stack
+        elif char == ')':
+            while stack[-1] != '(':
+                postfix += stack.pop() + " "
+            stack.pop()
+
+        # if the current character is not an operator
+        elif char not in order.keys():
+            postfix += char + " "
+
+        # if the current character IS an operator
+        else:
+            while stack and order[char] >= order[stack[-1]]:
+                postfix += stack.pop() + " "
+
+            stack.append(char)
+
+    while stack:
+        postfix += stack.pop() + " "
+
+    return postfix.strip()
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = [None, None]
+
+
+# Step 1: Create a data structure which can represent math expressions
+def makeExpressionTree(postfix):
+    root = None
+    stack = []
+
+    postfix = postfix.split(' ')
+
+    for char in postfix:
+        if char not in order:
+            stack.append(Node(char))
+
+        else:
+            node = Node(char)
+            node.children[1] = stack.pop()
+            node.children[0] = stack.pop()
+            stack.append(node)
+
+        root = stack[-1]
+    # return the root so you can begin traversing the tree
+    return root
+
+
+def evaluate(root):  # Step 2: Implement an evaluation method which calculates the value of the expression
+    val = root.value
+    if val not in order:
+        return int(val)
+
+    else:
+        left, right = evaluate(root.children[0]), evaluate(root.children[1])
+        return eval(str(left)+val+str(right))
+
+
+# Step 3: Write a parser which converts an ascii string to your data structure.
+# In order to turn the ascii to an expression tree, I have to convert the ascii to infix first
+def asciiToInfix(ascii):
+    is_binary = " " not in ascii
+    infix = ""
+    if is_binary:
+        ascii = wrap(ascii, 8)
+    else:
+        ascii = [int(a) for a in ascii.split(" ")]
+    for byte in ascii:
+        if is_binary:
+            byte = int(byte, 2)
+        infix += chr(byte)
+
+    return infix
--- tests/test_math_trees.py
+++ tests/test_math_trees.py
@@ -2,18 +2,45 @@
 
 import unittest
 
-from math_trees import math_trees
+from math_trees.math_trees import convertToPostfix, makeExpressionTree, evaluate, asciiToInfix
 
 
 class TestMathTrees(unittest.TestCase):
-    def test_basic_string(self):
+    def test_add_and_mult(self):
         math_string = "3 + 4 * 2"
-        # parse the string
-        # evaluate
-        # self.assertEqual(value, 11)
+        postfix = convertToPostfix(math_string)
+        tree = makeExpressionTree(postfix)
+        answer = evaluate(tree)
+        self.assertEqual(answer, 11)
 
-    def test_another_string(self):
+    def test_multiple_digits(self):
+        math_string = "3 + 14 * 2"
+        postfix = convertToPostfix(math_string)
+        tree = makeExpressionTree(postfix)
+        answer = evaluate(tree)
+        self.assertEqual(answer, 31)
+
+    def test_sub_and_divide(self):
         math_string = "8 - 9 / 3"
-        # parse the string
-        # evaluate
-        # self.assertEqual(value, 5)
+        postfix = convertToPostfix(math_string)
+        tree = makeExpressionTree(postfix)
+        answer = evaluate(tree)
+        self.assertEqual(answer, 5)
+
+    def test_binary_ascii(self):
+        # 8 + 11 / 3
+        binary_ascii = "00111000001000000010101100100000001100010011000100100000001011110010000000110011"
+        math_string = asciiToInfix(binary_ascii)
+        postfix = convertToPostfix(math_string)
+        tree = makeExpressionTree(postfix)
+        # check if the tree was made correctly for step 3
+        self.assertEqual(tree.value, "+")
+
+    def test_decimal_ascii(self):
+        # ( 2 + 2 ) / ( 6 - 10 )
+        decimal_ascii = "40 32 50 32 43 32 50 32 41 32 47 32 40 32 54 32 45 32 49 48 32 41"
+        math_string = asciiToInfix(decimal_ascii)
+        postfix = convertToPostfix(math_string)
+        tree = makeExpressionTree(postfix)
+        # check if the tree was made correctly for step 3
+        self.assertEqual(tree.value, "/")
